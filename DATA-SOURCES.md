e9c7a26c1463e2a82934d2bfcb2ed8bbe0e8a6a1
Markus Dymalla <markus.dymalla@beehiive.com>
2020-08-12 12:20:47 +1000

    Restructured data

--- DATA-SOURCES.md
+++ DATA-SOURCES.md
@@ -3,15 +3,6 @@
 ## ISO3166-2 codes & geographic centers
 https://github.com/oodavid/iso-3166-2/blob/master/iso_3166_2.js
 
-## ISO3166-2 codes
-https://github.com/sidm/laravel-subdivisions/blob/master/src/sidm/Subdivisions/Models/subdivisions.json
-
-## 1. ISO3166-2 codes
-https://github.com/alexander-schranz/iso-3166-2
-
-## ISO3166-1 codes
-https://github.com/thephpleague/iso3166
-
 ## ISO3166-1
 https://api.github.com/repos/umpirsky/country-list/contents/data
 
@@ -20,3 +11,9 @@ https://github.com/unicode-org/icu/tree/63e480dedcbff7208f21a6e2f516d6fbe215bc83
 
 ## ISO3166-1
 https://api.github.com/repos/lukes/ISO-3166-Countries-with-Regional-Codes/contents/all/all.json
+
+## ISO3166-2 language specific (licensed)
+https://github.com/esosedi/3166
+
+## ISO3166-1
+https://github.com/annexare/Countries/blob/master/data/countries.json
--- ISO3166-1-script.php
+++ ISO3166-1-script.php
@@ -4,127 +4,49 @@ require_once "vendor/autoload.php";
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7;
 
-$client = new Client(['auth' => ['MJDymalla']]);
+$client = new Client();
 
-// ISO3166-1 Create initial language based directories (ICU source)
-// missing heaps of codes?
-$response = $client->request('GET', 'https://api.github.com/repos/unicode-org/icu/contents/icu4c/source/data/region', [
-    'headers' => ['Accept' => 'application/vnd.github.v3+json'],
+// ISO3166: Initial files with Country and Region codes
+$response = $client->request('GET', 'https://api.github.com/repos/esosedi/3166/contents/data', [
+    'headers' => [ 'Accept' => 'application/vnd.github.v3+json' ]
 ]);
 
 $body = $response->getBody();
-$country_codes = json_decode($body, true);
+$data = json_decode($body, true);
 
-foreach ($country_codes as $country_code) {
-    $name = preg_replace('/\\.[^.\\s]{3,4}$/', '', $country_code["name"]);
-    $path = './ISO3166/'.$name;
-    mkdir($path);
-}
-echo "ISO3166-1 ICU Directories made\n\n";
-
-// ISO3166-1 Create files for each directory containing language and country specific data
-// will check if directory exists based on ICU data
-// (need to change how this works - will send 300+ requests)
-$response = $client->request('GET', 'https://api.github.com/repos/umpirsky/country-list/contents/data', [
-    'headers' => ['Accept' => 'application/vnd.github.v3+json'],
-]);
-
-$body = $response->getBody();
-$country_codes = json_decode($body, true);
-
-
-foreach ($country_codes as $country_code) {
-
-    $res = $client->request('GET', $country_code["url"], [
-        'headers' => ['Accept' => 'application/vnd.github.v3+json']
-    ]);
-
-    $bod = $res->getBody();
-    $codes = json_decode($bod, true);
-
-    foreach ($codes as $code) {
-
-        if ($code["name"] == "country.json") {
-            $countries = json_decode(file_get_contents($code["download_url"]), true);
-            $path = 'ISO3166/'.$country_code["name"];
-
-            if (!file_exists($path)) {
-                echo "Directory doesn't exist: ".$country_code["name"]."\n";
-                continue;
-            }
-
-            foreach ($countries as $key => $value) {
-                $file_name = strtolower($key);
-
-                $country = ['name' => $value,
-                    'alpha-2' => $key,
-                    'alpha-3' => null,
-                    'country-code' => null,
-                    'region' => null,
-                    'ISO3166-2' => []
-                ];
-
-                file_put_contents($path.'/'.$file_name.'.json', json_encode($country, JSON_PRETTY_PRINT));
-            }
-        }
-    }
-}
-echo "ISO3166-1 Files made with country objects\n\n";
-
-//ISO3166-1 country codes (name, alpha-2, alpha-3 code, region)
-$response = $client->request('GET', 'https://api.github.com/repos/lukes/ISO-3166-Countries-with-Regional-Codes/contents/all/all.json', [
-    'headers' => ['Accept' => 'application/vnd.github.v3+json'],
-]);
-
-$body = $response->getBody();
-$country_codes = json_decode($body, true);
-
-$json = file_get_contents($country_codes["download_url"]);
-$countries = json_decode($json, true);
+$countries = json_decode(file_get_contents($data[0]["download_url"]), true);
 
 foreach ($countries as $country) {
-    foreach(new DirectoryIterator('./ISO3166') as $directory) {
-        $name = strtolower($country["alpha-2"]);
-        $path = './ISO3166/'.$directory.'/'.$name.'.json';
+    $country_code = strtolower($country["iso"]);
+    $file_name = $country_code.'.json';
+    $path = 'ISO3166/'.$file_name;
 
-        if (file_exists($path)) {
-            $existing_contents = file_get_contents($path);
-            $decoded = json_decode($existing_contents, true);
+    $country_object = ['alpha-2' => $country["iso"],
+        'alpha-3' => $country["iso3"],
+        'numeric' => $country["numeric"],
+        'capital' => null,
+        'currency' => null,
+        'names' => $country["names"],
+        'languages' => null,
+        'ISO3166-2' => null
+    ];
 
-            $decoded["alpha-3"] = $country["alpha-3"];
-            $decoded["region"] = $country["region"];
-            $decoded["country-code"] = $country["country-code"];
-            file_put_contents($path, json_encode($decoded, JSON_PRETTY_PRINT));
-        }
+    foreach ($country["regions"] as $region) {
+        $country_object['ISO3166-2'][$region['iso']] = ['name' => $region["name"],
+            'code' => $region["iso"],
+            'names' => $region["names"],
+            'division' => null,
+            'parent' => null,
+            'lat' => null,
+            'lng' => null
+        ];
     }
+
+    file_put_contents($path, json_encode($country_object, JSON_PRETTY_PRINT));
 }
-echo "ISO3166-1 Extra country data added\n\n";
+echo "ISO3166: Initial files created with Country and Region data\n\n";
 
-//// ISO3166-2: Set initial subdivisions
-//$response = $client->request('GET', 'https://api.github.com/repos/alexander-schranz/iso-3166-2/contents/subdivisions',
-//    [ 'headers' => [ 'Accept' => 'application/vnd.github.v3+json' ]]
-//);
-//
-//$body = $response->getBody();
-//$country_codes = json_decode($body, true);
-//
-//foreach($country_codes as $country_code) {
-//    if ($country_code["type"] == "file") {
-//        $file_name = $country_code["name"];
-//        $path = './ISO3166/en_AU/'.$file_name;
-//
-//        $existing_contents = file_get_contents($path);
-//        $decoded = json_decode($existing_contents, true);
-//
-//        $contents = file_get_contents($country_code["download_url"]);
-//        $decode = json_decode($contents, true);
-//
-//        $decoded["ISO3166-2"] = $decode;
-//        file_put_contents($path, json_encode($decoded, JSON_PRETTY_PRINT));
-//    }
-//}
-
-// ISO3166-2: Source 2
+// ISO3166-2: Extra region data (division, lat, lng)
 $response = $client->request('GET', 'https://api.github.com/repos/oodavid/iso-3166-2/contents/iso_3166_2.js', [
     'headers' => [ 'Accept' => 'application/vnd.github.v3+json' ]
 ]);
@@ -149,22 +71,55 @@ $json = implode("\n", array_map(function (string $line) {
 $codes = json_decode($json, true);
 
 foreach ($codes as $code) {
-    // needs substring of country code in lower case alpha-2 to compare to files
     $country_code = substr($code["code"], 0, 2);
-    $file_name = strtolower($country_code);
+    $region_code = substr($code["code"], 3);
 
-    $path = './ISO3166/en_AU/'.$file_name.'.json';
+    $file_name = strtolower($country_code);
+    $path = './ISO3166/'.$file_name.'.json';
 
     if (file_exists($path) && false !== strpos($code["code"], "-")) {
         $existing_contents = file_get_contents($path);
         $decoded = json_decode($existing_contents, true);
 
-        array_push($decoded["ISO3166-2"], $code);
+        if (!isset($decoded['ISO3166-2'][$region_code]['name'])) {
+            $decoded['ISO3166-2'][$region_code]['name'] = $code['name'];
+        }
+
+        $decoded['ISO3166-2'][$region_code]['division'] = $code['division'];
+        $decoded['ISO3166-2'][$region_code]['parent'] = $code['parent'];
+
+        if (isset($code['lat']) && isset($code['lng'])) {
+            $decoded['ISO3166-2'][$region_code]['lat'] = $code['lat'];
+            $decoded['ISO3166-2'][$region_code]['lng'] = $code['lng'];
+        }
+
         file_put_contents($path, json_encode($decoded, JSON_PRETTY_PRINT));
     }
 }
-echo "ISO3166-2 Codes added to objects in en_AU\n\n";
+echo "ISO3166-2: Division, lat, and lng data added\n\n";
 
+// ISO3166-1: Additional data and checks
+$response = $client->request('GET', 'https://api.github.com/repos/annexare/Countries/contents/data/countries.json', [
+    'headers' => [ 'Accept' => 'application/vnd.github.v3+json' ]
+]);
 
+$body = $response->getBody();
+$data = json_decode($body, true);
+$countries = json_decode(file_get_contents($data["download_url"]), true);
 
+foreach ($countries as $key => $value) {
+    $country_code = strtolower($key);
+    $path = './ISO3166/'.$country_code.'.json';
 
+    if (file_exists($path)) {
+        $existing_contents = file_get_contents($path);
+        $decoded = json_decode($existing_contents, true);
+
+        $decoded['capital'] = $value['capital'];
+        $decoded['currency'] = $value['currency'];
+        $decoded['languages'] = $value['languages'];
+
+        file_put_contents($path, json_encode($decoded, JSON_PRETTY_PRINT));
+    }
+}
+echo "ISO3166-1: Country capital, currencies, and languages data added\n\n";
