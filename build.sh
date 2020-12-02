9c4173f5fcd6ceac6b31e9cb8025230123ff6f65
Joe Bennett <joe.bennett@beehiive.com>
2020-12-02 14:33:32 +1000

    Improved build process

--- build.sh
+++ build.sh
@@ -2,17 +2,5 @@
 
 DOCKER_IMAGE=mjdymalla/iso3166-library
 
-if [ $1 = "rebuild" ]
-then
-    docker build --no-cache -t $DOCKER_IMAGE .
-else
-    docker build -t $DOCKER_IMAGE .
-fi
-
-sudo apt install composer
-
-composer update
-
-composer install
-
-docker run --rm -it $DOCKER_IMAGE
+docker build -t $DOCKER_IMAGE .
+docker run --rm -it $DOCKER_IMAGE $@
